63c53200fb7db7b11d2e7c73bd9db545555d4186
Brian Savage <brianrsavage@users.noreply.github.com>
2018-10-31 17:41:49 -0400

    Update index.php

--- series/get-ip-better/index.php
+++ series/get-ip-better/index.php
@@ -5,9 +5,11 @@ $http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $remote_addr = $_SEVER['REMOTE_ADDR'];
 
 
-//01. Explain what the following conditional statment is doing.
-//It ischecking to see if there is an ip addres and if there is it sets it to be the client ip address.
-// Then if it is not then it checks for http_x_forwarded and if that is empty as well it makes the ip address the remote one.
+  /*
+    01. Explain what the following conditional statment is doing.
+    It ischecking to see if there is an ip addres and if there is it sets it to be the client ip address.
+    Then if it is not then it checks for http_x_forwarded and if that is empty as well it makes the ip address the remote one.
+  */ 
 if (!empty($http_client_ip)) {
   $ip_address = $http_client_ip;
 } else if (!empty($http_x_forwarded_for)) {
